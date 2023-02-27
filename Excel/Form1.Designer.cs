5635dbdbd23410d1751cee70cb8bcc9cfedf820d
Yasmin Hannah <72814378+yasminhannahza@users.noreply.github.com>
2023-02-27 16:51:47 +0800

    Add project files.

---
+++ Excel/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace Excel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.openButton = new System.Windows.Forms.Button();
+            this.totalRecordLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AllowUserToAddRows = false;
+            this.dataGridView.AllowUserToDeleteRows = false;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.ReadOnly = true;
+            this.dataGridView.RowHeadersWidth = 51;
+            this.dataGridView.RowTemplate.Height = 24;
+            this.dataGridView.Size = new System.Drawing.Size(786, 430);
+            this.dataGridView.TabIndex = 3;
+            // 
+            // openButton
+            // 
+            this.openButton.Location = new System.Drawing.Point(723, 448);
+            this.openButton.Name = "openButton";
+            this.openButton.Size = new System.Drawing.Size(75, 31);
+            this.openButton.TabIndex = 4;
+            this.openButton.Text = "Open";
+            this.openButton.UseVisualStyleBackColor = true;
+            this.openButton.Click += new System.EventHandler(this.openButton_Click);
+            // 
+            // totalRecordLabel
+            // 
+            this.totalRecordLabel.AutoSize = true;
+            this.totalRecordLabel.Location = new System.Drawing.Point(9, 451);
+            this.totalRecordLabel.Name = "totalRecordLabel";
+            this.totalRecordLabel.Size = new System.Drawing.Size(96, 16);
+            this.totalRecordLabel.TabIndex = 0;
+            this.totalRecordLabel.Text = "Total Records:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(808, 485);
+            this.Controls.Add(this.openButton);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.totalRecordLabel);
+            this.Name = "Form1";
+            this.Text = "Read Excel File";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Button openButton;
+        private System.Windows.Forms.Label totalRecordLabel;
+    }
+}
+
---
+++ Excel/Form1.cs
@@ -0,0 +1,62 @@
+﻿using ClosedXML.Excel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Excel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openButton_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog() { Filter = "Excel workbook|*.xlsx", Multiselect = false })
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    Cursor.Current = Cursors.WaitCursor;
+                    DataTable dt = new DataTable();
+                    using(XLWorkbook workbook = new XLWorkbook(ofd.FileName)) 
+                    {
+                        bool isFirstRow = true;
+                        var rows = workbook.Worksheet(1).RowsUsed();
+                        foreach(var row in rows)
+                        {
+                            if (isFirstRow)
+                            {
+                                foreach (IXLCell cell in row.Cells())
+                                    dt.Columns.Add(cell.Value.ToString());
+                                isFirstRow = false;
+                            }
+                            else
+                            {
+                                dt.Rows.Add();
+                                int i = 0;
+                                foreach (IXLCell cell in row.Cells())
+                                    dt.Rows[dt.Rows.Count - 1][i++] = cell.Value.ToString();
+                            }
+                        }
+                        dataGridView.DataSource = dt.DefaultView;
+                        totalRecordLabel.Text = $"Total Records: {dataGridView.Rows.Count}";
+                        Cursor.Current = Cursors.Default;
+                    }
+                }
+            }
+        }
+    }
+}
